f8982ba78f9eaaf2d8f264a84bd2dde6a98acb44
Luca Di Gianni <luca.dgni@gmail.com>
2024-01-15 16:33:25 +0100

    Refactor email validation

--- script.js
+++ script.js
@@ -2,37 +2,43 @@
 const subscribeForm = document.getElementById("subscribe-form");
 const emailInput = document.getElementById("email-input");
 
-// Create the error and success messages
+// Create variables to store the error and success messages
 let errorMessage = null;
 let successMessage = null;
 
-// Add an event listener to the form
-subscribeForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+// Create a regular expression to validate the email address
+const emailValidationRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-  if (emailRegex.test(emailInput.value)) {
+// Function to create a message
+function createAlertMessage(type, text) {
+  const message = document.createElement("p");
+  message.textContent = text;
+  message.classList.add("msg", `msg--${type}`);
+  message.setAttribute("role", "alert");
+  return message;
+}
+
+// Add an event listener to the form
+subscribeForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (emailValidationRegex.test(emailInput.value)) {
     // Remove the error message if it exists
     if (errorMessage) {
+      emailInput.classList.remove("input--error");
       errorMessage.remove();
       errorMessage = null;
     }
 
     // Only create the success message if it doesn't already exist
     if (!successMessage) {
-      successMessage = document.createElement("p");
-      successMessage.textContent = "Thanks for subscribing";
-      successMessage.classList.add("msg");
-      successMessage.classList.add("msg--success");
-      successMessage.setAttribute("role", "alert");
-      emailInput.setAttribute("aria-invalid", "false");
+      successMessage = createAlertMessage("success", "Thanks for subscribing");
+      emailInput.removeAttribute("aria-describedby");
+      emailInput.removeAttribute("aria-invalid");
       subscribeForm.appendChild(successMessage);
+      emailInput.value = "";
+      emailInput.blur();
     }
-
-    // Change the border color of the input field and display the success message
-    emailInput.style.borderColor = "#c2d3ff";
-    successMessage.style.display = "block";
-    emailInput.value = "";
   } else {
     // Remove the success message if it exists
     if (successMessage) {
@@ -42,17 +48,16 @@ subscribeForm.addEventListener("submit", function (e) {
 
     // Only create the error message if it doesn't already exist
     if (!errorMessage) {
-      errorMessage = document.createElement("p");
-      errorMessage.textContent = "Please provide a valid email address";
-      errorMessage.classList.add("msg");
-      errorMessage.classList.add("msg--error");
-      errorMessage.setAttribute("role", "alert");
+      errorMessage = createAlertMessage(
+        "error",
+        "Please provide a valid email address"
+      );
+      errorMessage.setAttribute("id", "error");
+      emailInput.classList.add("input--error");
+      emailInput.setAttribute("aria-describedby", "error");
       emailInput.setAttribute("aria-invalid", "true");
       subscribeForm.appendChild(errorMessage);
+      emailInput.blur();
     }
-
-    // Change the border color of the input field and display the error message
-    emailInput.style.borderColor = "#FF5466";
-    errorMessage.style.display = "block";
   }
 });
